d7739e3d508cf4bf81c31c85ee12089272b533e5
Matija Kosanović <matija.kosanovic@koncar-ket.hr>
2018-03-08 12:48:56 +0100

    Added filter to copy task

--- vars/copy.groovy
+++ vars/copy.groovy
@@ -28,10 +28,5 @@ def call(Map parameters = [:]){
 def antFunction(String toDir, String fromDir, String filter, boolean flatten){
 	def ant =new AntBuilder()
 
-	ant.copy(toDir:toDir,overwrite:true,flatten:flatten, verbose:true){
-		fileset(dir:fromDir){
-			include(name:filter)
-			exclude(name:"**/.git/**/*.*")
-		}
-	}
+	ant.copy(toDir:toDir,overwrite:true,flatten:flatten, verbose:true){fileset(dir:fromDir){include(name:filter)exclude(name:"**/.git/**/*.*")}}
 }
