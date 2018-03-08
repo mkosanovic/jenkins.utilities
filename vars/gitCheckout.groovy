46376d7eb9614d805d18b17e3273d4d4ad39c928
Matija Kosanović <matija.kosanovic@koncar-ket.hr>
2018-03-08 10:38:20 +0100

    syntax error

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,4 +1,4 @@
-def call(Map parameters = [:])
+def call(Map parameters = [:]){
 	// String repo, String relativeTargetDir, String credentials, String branch= '*/master',  String refspec='+refs/heads/master:refs/remotes/origin/master'){
 
 	def credentials = parameters.containsKey("credentials") ? parameters.credentials : "$env.GIT_CREDENTIALS"
