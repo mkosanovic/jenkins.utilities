6d1ce097540c205e099f5735eb71d6b30052196b
Matija Kosanović <matija.kosanovic@koncar-ket.hr>
2018-03-08 12:22:57 +0100

    Added copy task

---
+++ vars/copy.groovy
@@ -0,0 +1,30 @@
+def call(Map parameters = [:]){
+	def ant = new AntBuilder()
+
+	def file = parameters.file
+	def toFile = parameters.toFile
+	def fromDir = parameters.fromDir
+	def toDir = parameters.toDir
+	def flatten = parameters.flatten ? parameters.flatten : true // true by default
+
+	if(file && toFile)
+	{
+		ant.copy(file:file, toFile:toFile,overwrite:true,flatten:flatten, verbose:true)
+	}
+	else if(file && toDir)
+	{
+		ant.copy(file:file, toDir:toFile,overwrite:true,flatten:flatten, verbose:true)
+	}
+	else if(fromDir && toDir)
+	{
+		ant.copy(toDir:toDir,overwrite:true,flatten:flatten, verbose:true){
+			fileset(dir:fromDir){
+				include(name:filter)
+				exclude(name:"**/.git/**/*.*")
+			}
+		}
+	}
+	else{
+		echo "Incorrect input arguments"
+	}
+}
