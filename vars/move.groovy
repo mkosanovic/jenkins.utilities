70c5e9182b5230657798307a510035d3f80b878c
Matija Kosanović <matija.kosanovic@koncar-ket.hr>
2018-03-13 14:39:35 +0100

    Added exception handling

--- vars/move.groovy
+++ vars/move.groovy
@@ -7,23 +7,30 @@ def call(Map parameters = [:]){
 	def toDir = parameters.toDir
 	def flatten = parameters.flatten ? parameters.flatten : true // true by default
 	def filter = parameters.filter
+	def failOnException = parameters.failOnException ? parameters.failOnException : false
 
-	if(file && toFile)
-	{
-		ant.move(file:file, toFile:toFile,verbose:true)
-	}
-	else if(file && toDir)
-	{
-		ant.move(file:file, toDir:toDir,flatten:flatten, verbose:true)
-	}
-	else if(fromDir && toDir && filter)
-	{
-		println "Not implemented"
-		// antFunction(toDir,fromDir,filter,flatten)
-	}
-	else{
-		echo "Incorrect input arguments"
-	}
+	try{
+		if(file && toFile)
+		{
+			ant.move(file:file, toFile:toFile,verbose:true)
+		}
+		else if(file && toDir)
+		{
+			ant.move(file:file, toDir:toDir,flatten:flatten, verbose:true)
+		}
+		else if(fromDir && toDir && filter)
+		{
+			println "Not implemented"
+			// antFunction(toDir,fromDir,filter,flatten)
+		}
+		else{
+			echo "Incorrect input arguments"
+		}
+	}catch(e){
+		echo e
+
+		if(failOnException){ throw e; }
+	}	
 }
 
 def antFunction(String toDir, String fromDir, String filter, boolean flatten){	
